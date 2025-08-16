3549bb82f22b6682d3d2def6e7b31e48c752230e
Kutman878 <abdalievkutman2@gmail.com>
2025-08-16 23:58:39 +0600

    доработка энпоинтов и подключение к БД

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -5,7 +5,7 @@ namespace ToDoApi.Models
     public class TaskItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string Status { get; set; } = "pending";
         public string Priority { get; set; } = "low";
